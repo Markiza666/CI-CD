907168da372fc1c66a37482cd101a0dfcc371da1
Kentska <kent.skarnaes@gmail.com>
2025-11-10 18:41:55 +0100

    server log 3

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -66,8 +66,8 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 app.listen(PORT, () => {
 	console.log(`🚀 API running on port ${PORT}`);
 });*/
-const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
+const PORT = process.env.PORT || "5000";
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
 	console.log(`🚀 API running on port ${PORT}`);
 });
