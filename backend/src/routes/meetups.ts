6f3a49dedb913c67a7ef85e943ca19a7db3f1d6d
Kentska <kent.skarnaes@gmail.com>
2025-11-13 01:52:38 +0100

    change of column names

--- backend/src/routes/meetups.ts
+++ backend/src/routes/meetups.ts
@@ -117,7 +117,7 @@ router.post("/:id/register", auth, async (req: Request, res: Response) => {
 	const capacityCheck = await db.query(
 		`
    SELECT max_capacity, (
-     SELECT COUNT(*) FROM registrations WHERE meetupid = $1
+     SELECT COUNT(*) FROM registrations WHERE meetup_id = $1
    ) AS current FROM meetups WHERE id = $1
 `,
 		[meetupId]
@@ -134,7 +134,7 @@ router.post("/:id/register", auth, async (req: Request, res: Response) => {
 
 	const { v4: uuidv4 } = await import("uuid"); //  funkar i CJS
 	await db.query(
-		`INSERT INTO registrations (id, userid, meetupid)
+		`INSERT INTO registrations (id, user_id, meetup_id)
   VALUES ($1, $2, $3)`,
 		[uuidv4(), req.userId, meetupId]
 	);
@@ -163,7 +163,7 @@ router.post("/", auth, async (req: Request, res: Response) => {
 router.delete("/:id/register", auth, async (req: Request, res: Response) => {
 	const userId = req.userId;
 	const result = await db.query(
-		`DELETE FROM registrations WHERE meetupid = $1 AND userid = $2`,
+		`DELETE FROM registrations WHERE meetup_id = $1 AND user_id = $2`,
 		[req.params.id, userId]
 	);
 	if (result.rowCount === 0)
