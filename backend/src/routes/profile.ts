c5f82cc6625e778eba0954d8653d9ef05ed407f0
Kentska <kent.skarnaes@gmail.com>
2025-11-13 03:29:47 +0100

    1 more time

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -10,8 +10,8 @@ const router = Router();
 router.get('/meetups', auth, async (req: Request, res: Response) => {
     const result = await db.query(`
     SELECT m.* FROM meetups m
-    JOIN registrations r ON r.meetupid = m.id
-    WHERE r.userid = $1`
+    JOIN registrations r ON r.meetup_id = m.id
+    WHERE r.user_id = $1`
   , [req.userId]);
     res.json(result.rows);
 });
